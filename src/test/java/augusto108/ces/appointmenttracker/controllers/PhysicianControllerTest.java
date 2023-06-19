97845c64e067a66c8bf71f37966cac1d3a49c7ab
Cesar Augusto <ces.augusto108@gmail.com>
2023-06-19 16:15:22 -0300

    PhysicianController savePhysician test update and test class refactoring

--- src/test/java/augusto108/ces/appointmenttracker/controllers/PhysicianControllerTest.java
+++ src/test/java/augusto108/ces/appointmenttracker/controllers/PhysicianControllerTest.java
@@ -2,7 +2,6 @@ package augusto108.ces.appointmenttracker.controllers;
 
 import augusto108.ces.appointmenttracker.model.Physician;
 import augusto108.ces.appointmenttracker.model.enums.Specialty;
-import augusto108.ces.appointmenttracker.repositories.PhysicianRepository;
 import augusto108.ces.appointmenttracker.security.Employee;
 import augusto108.ces.appointmenttracker.services.EmployeeService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -25,7 +24,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
-import javax.persistence.EntityManager;
 import javax.servlet.Filter;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -33,7 +31,6 @@ import java.util.List;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -43,9 +40,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Transactional
 @TestPropertySource("classpath:users.properties")
 class PhysicianControllerTest {
-    @Autowired
-    private PhysicianController physicianController;
-
     private MockMvc mockMvc;
 
     @Autowired
@@ -55,16 +49,10 @@ class PhysicianControllerTest {
     private ObjectMapper objectMapper;
 
     @Autowired
-    private EntityManager entityManager;
-
-    @Autowired
     private Filter springSecurityFilterChain;
 
     @Autowired
     private EmployeeService employeeService;
-
-    @Autowired
-    private PhysicianRepository physicianRepository;
 
     @Value("${users.pword}")
     private String empPassword;
@@ -132,8 +120,7 @@ class PhysicianControllerTest {
                 .andExpect(jsonPath("$.firstName", is("João")))
                 .andExpect(jsonPath("$.lastName", is("Brito")))
                 .andExpect(jsonPath("$.specialty", is("DERMATOLOGIST")))
-                .andExpect(jsonPath("$.appointments", hasSize(3)))
-                .andReturn();
+                .andExpect(jsonPath("$.appointments", hasSize(3)));
     }
 
     @Test
@@ -143,15 +130,15 @@ class PhysicianControllerTest {
         physician.setLastName("Muntz");
         physician.setSpecialty(Specialty.ENDOCRINOLOGIST);
 
-        entityManager.persist(physician);
-
         mockMvc.perform(post("/physicians").with(makeAuthorizedAdminUser())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(physician)))
-                .andExpect(status().isCreated());
-
-
-        assertNotNull(physicianRepository.findById(11L));
+                .andExpect(content().contentType("application/hal+json"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", is(101)))
+                .andExpect(jsonPath("$.firstName", is("Nelson")))
+                .andExpect(jsonPath("$.lastName", is("Muntz")))
+                .andExpect(jsonPath("$.specialty", is("ENDOCRINOLOGIST")));
     }
 
     private RequestPostProcessor makeAuthorizedAdminUser() {
