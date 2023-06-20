f2bc2a4b5929aab77f9b00ad6238b60ad8b85127
César Augusto <ces.augusto108@gmail.com>
2023-06-20 08:08:03 -0300

    PatientController tests

--- src/test/java/augusto108/ces/appointmenttracker/controllers/PatientControllerTest.java
+++ src/test/java/augusto108/ces/appointmenttracker/controllers/PatientControllerTest.java
@@ -1,10 +1,12 @@
 package augusto108.ces.appointmenttracker.controllers;
 
+import augusto108.ces.appointmenttracker.model.Patient;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -17,6 +19,7 @@ import java.util.LinkedHashMap;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -56,6 +59,7 @@ class PatientControllerTest extends AuthorizeAdminUser {
                 .andExpect(jsonPath("$.page.totalElements", is(12)))
                 .andExpect(jsonPath("$.page.totalPages", is(1)))
                 .andExpect(jsonPath("$.page.number", is(0)))
+                .andExpect(jsonPath("$._links.self.href", is(selfLink)))
                 .andReturn();
 
         final String content = result.getResponse().getContentAsString();
@@ -65,14 +69,55 @@ class PatientControllerTest extends AuthorizeAdminUser {
     }
 
     @Test
-    void searchPatients() {
+    void searchPatients() throws Exception {
+        final String selfLink = "http://localhost/patients/search?page=0&size=20&sort=id,asc";
+
+        MvcResult result = mockMvc.perform(get("/patients/search").with(makeAuthorizedAdminUser())
+                        .param("search", "Silva")
+                        .param("page", "0")
+                        .param("size", "20")
+                        .param("direction", "ASC")
+                        .param("field", "id"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/hal+json"))
+                .andExpect(jsonPath("$.page.size", is(20)))
+                .andExpect(jsonPath("$.page.totalElements", is(2)))
+                .andExpect(jsonPath("$.page.totalPages", is(1)))
+                .andExpect(jsonPath("$.page.number", is(0)))
+                .andExpect(jsonPath("$._links.self.href", is(selfLink)))
+                .andReturn();
+
+        final String content = result.getResponse().getContentAsString();
+        final int pageJsonKeySetSize = objectMapper.readValue(content, LinkedHashMap.class).keySet().size();
+
+        assertEquals(3, pageJsonKeySetSize);
     }
 
     @Test
-    void getPatientById() {
+    void getPatientById() throws Exception {
+        mockMvc.perform(get("/patients/{id}", 3).with(makeAuthorizedAdminUser()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/hal+json"))
+                .andExpect(jsonPath("$.firstName", is("Hugo")))
+                .andExpect(jsonPath("$.lastName", is("Silva")))
+                .andExpect(jsonPath("$.email", is("hugo@email.com")));
     }
 
     @Test
-    void savePatient() {
+    void savePatient() throws Exception {
+        final Patient patient = new Patient();
+        patient.setFirstName("Leonardo");
+        patient.setLastName("Ribeiro");
+        patient.setEmail("leonardo@email.com");
+
+        mockMvc.perform(post("/patients").with(makeAuthorizedAdminUser())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(patient)))
+                .andExpect(content().contentType("application/hal+json"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", is(101)))
+                .andExpect(jsonPath("$.firstName", is("Leonardo")))
+                .andExpect(jsonPath("$.lastName", is("Ribeiro")))
+                .andExpect(jsonPath("$.email", is("leonardo@email.com")));
     }
 }
