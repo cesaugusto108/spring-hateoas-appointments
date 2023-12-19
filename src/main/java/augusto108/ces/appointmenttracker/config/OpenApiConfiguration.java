da2e52f4bc90da8d5ec1a2f9ffd75196591392fc
Cesar Augusto <cesaraugusto@skiff.com>
2023-12-19 16:40:21 -0300

    OpenApi configuration refactoring - cleanup and style

--- src/main/java/augusto108/ces/appointmenttracker/config/OpenApiConfiguration.java
+++ src/main/java/augusto108/ces/appointmenttracker/config/OpenApiConfiguration.java
@@ -8,12 +8,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfiguration {
+
     @Bean
     public OpenAPI openAPIconfig() {
         final Info info = new Info();
         final String version = VersioningConstant.VERSION.substring(5, 7);
-
-        return new OpenAPI()
-                .info(info.version(version).title("Appointment tracker").description("Appointments managing system"));
+        final String title = "Medical Appointment Tracker";
+        final String description = "Medical Appointments managing system";
+        return new OpenAPI().info(info.version(version).title(title).description(description));
     }
 }
