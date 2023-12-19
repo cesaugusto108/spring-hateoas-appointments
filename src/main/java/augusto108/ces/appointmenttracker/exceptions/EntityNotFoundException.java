b163a11a2d4111b4abf6e15edb4effada6600a2a
Cesar Augusto <cesaraugusto@skiff.com>
2023-12-19 20:19:58 -0300

    Custom exceptions refactoring - cleanup and style

--- src/main/java/augusto108/ces/appointmenttracker/exceptions/EntityNotFoundException.java
+++ src/main/java/augusto108/ces/appointmenttracker/exceptions/EntityNotFoundException.java
@@ -1,6 +1,7 @@
 package augusto108.ces.appointmenttracker.exceptions;
 
 public class EntityNotFoundException extends RuntimeException {
+
     public EntityNotFoundException(String message) {
         super(message);
     }
