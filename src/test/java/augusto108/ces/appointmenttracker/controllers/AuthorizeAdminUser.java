141f0cec17a0c0429debb16b9f2bcaf8e7eb2d8f
Cesar Augusto <cesaraugusto@skiff.com>
2023-12-19 12:32:14 -0300

    Fixed class used by tests - set property source value

--- src/test/java/augusto108/ces/appointmenttracker/controllers/AuthorizeAdminUser.java
+++ src/test/java/augusto108/ces/appointmenttracker/controllers/AuthorizeAdminUser.java
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.request.RequestPostProcessor;
 
 import java.util.List;
 
+@PropertySource(value = "classpath:users.properties")
 public abstract class AuthorizeAdminUser {
     @Autowired
     private EmployeeService employeeService;
