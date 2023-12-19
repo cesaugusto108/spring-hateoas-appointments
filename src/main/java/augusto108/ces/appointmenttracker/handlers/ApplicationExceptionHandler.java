fcb35982f6e1e1d35c58594656f08fcb1ed1114f
Cesar Augusto <cesaraugusto@skiff.com>
2023-12-19 16:52:51 -0300

    Exception handlers refactoring - cleanup and style

--- src/main/java/augusto108/ces/appointmenttracker/handlers/ApplicationExceptionHandler.java
+++ src/main/java/augusto108/ces/appointmenttracker/handlers/ApplicationExceptionHandler.java
@@ -39,6 +39,7 @@ public class ApplicationExceptionHandler {
 
     @Getter
     public static class ErrorResponse {
+
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
         private final LocalDateTime timestamp;
 
